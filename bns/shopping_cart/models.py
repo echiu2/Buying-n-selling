7c2007e82b80333f9f7f62cfe3871790671bc901
Edwin Chiu <echiu2@buffalo.edu>
2020-06-01 19:32:50 -0400

    implemented shopping cart basic UI and changed cart Models

--- bns/shopping_cart/models.py
+++ bns/shopping_cart/models.py
@@ -9,8 +9,11 @@ from listings.models import Post
 
 class Cart(models.Model):
     buyer = models.OneToOneField(User, null=True, blank=False, on_delete=models.CASCADE)
-    product = models.ManyToManyField(Post, blank=True)
+    product = models.ForeignKey(Post, null=True, on_delete=models.CASCADE)
     subtotal = models.DecimalField(default=0.00, decimal_places=2, max_digits=100)
     final_total = models.DecimalField(default=0.00, decimal_places=2, max_digits=100) 
     amount = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f' {self.buyer} order'
 
--- bns/shopping_cart/views.py
+++ bns/shopping_cart/views.py
@@ -4,7 +4,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Cart
 
 # Create your views here.
 def cart(request):
-    return render(request, 'shopping_cart/cart.html')
+    cart = Cart.objects.all()
+    context = {'Cart': cart}
+    return render(request, 'shopping_cart/cart.html', context)
